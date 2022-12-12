c7dc68f10cc96e2771b30b3f6d31a990b832c61e
Anton <tosha.hd2@gmail.com>
2022-12-12 22:23:32 +0500

    task2

--- src/hw2.java
+++ src/hw2.java
@@ -5,9 +5,12 @@ public class hw2 {
     public static void main(String[] args) {
         //1. Напишите программу, чтобы найти наименьшее окно в строке, содержащей все символы другой строки.
         task1();
+        //2. Напишите программу, чтобы проверить, являются ли две данные строки вращением друг друга.
+        task2();
 
     }
     static void task1 () {
+        System.out.println("Задача 1");
         String stringFirst = "строка тут";
         String stringSecond = "Вторая строка тут";
         System.out.println("Ищем наименьшее окно в строке '" + stringSecond + "', содержащее все символы строки '" + stringFirst + "'.");
@@ -55,5 +58,13 @@ public class hw2 {
             }
         }
         System.out.println("Наименьшее окно в строке '" + stringSecond + "' следующее: '" + result + "'.");
+    }
+    static void task2 () {
+        System.out.println("\nЗадача 2");
+        StringBuilder stringForward = new StringBuilder("как ротор ни крути");
+        String stringBackward = "итурк ин ротор как";
+        String reverseString = new String(stringForward.reverse().toString());
+        String result = new String(reverseString.equals(stringBackward) ? "является" : "не является");
+        System.out.println("'" + stringForward.reverse() + "' " + result + " вращением '" + stringBackward + "'");
     }
 }
